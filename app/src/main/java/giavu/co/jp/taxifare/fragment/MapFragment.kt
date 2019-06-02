5112805696e4a1499fac004c332f12e892e69af4
mihovu <hoangvmh@gmail.com>
2019-06-02 15:03:56 +0900

    Add MapFragment

--- app/src/main/java/giavu/co/jp/taxifare/fragment/MapFragment.kt
+++ app/src/main/java/giavu/co/jp/taxifare/fragment/MapFragment.kt
@@ -1,9 +1,11 @@
 package giavu.co.jp.taxifare.fragment
 
+import com.google.android.m4b.maps.SupportMapFragment
+
 /**
  * @Author: Hoang Vu
  * @Date:   2019-05-31
  */
-class MapFragment {
+class MapFragment : SupportMapFragment() {
 
 }
